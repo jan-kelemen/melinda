35d724e76090c0815f6117e49d2468a1dc49ca15
Jan Kelemen <jkeleme@gmail.com>
2019-07-13 18:00:56 +0200

    travis tryout

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,3 +1,5 @@
+#include "../commmon/include/iterator_traits.h"
+
 #include <iostream>
 
 int main() { std::cout << "Hello world!\n"; }
---
+++ travis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pushd "source" && . release.sh && popd
