a186dda9a2cb13209c036a7a5142a0b22b900bda
Jan Kelemen <jkeleme@gmail.com>
2020-08-08 19:09:15 +0200

    Improve error handling on the server

--- source/client/src/main.cpp
+++ source/client/src/main.cpp
@@ -53,6 +53,6 @@ int main()
             std::cout << "Recevied message: " << message.to_string_view()
                       << '\n';
         }
-        std::this_thread::sleep_for(std::chrono::seconds(3));
+        //        std::this_thread::sleep_for(std::chrono::seconds(3));
     }
 }
--- source/server/src/main.cpp
+++ source/server/src/main.cpp
@@ -1,6 +1,7 @@
 #include "../../common/cppex/include/scope_exit.h"
 #include "../../common/trace/include/trace.h"
 
+#include <algorithm>
 #include <array>
 #include <iostream>
 #include <vector>
@@ -20,16 +21,19 @@ int main()
     zmq::context_t ctx;
     zmq::socket_t socket = zmq::socket_t(ctx, zmq::socket_type::router);
 
-    constexpr int linger = 0;
-    socket.setsockopt(ZMQ_LINGER, linger);
-    socket.setsockopt(ZMQ_ROUTER_MANDATORY, 1);
+    socket.setsockopt(ZMQ_LINGER, 0); // Close the socket immediately
+    socket.setsockopt(ZMQ_ROUTER_MANDATORY,
+        1); // Report host unreachable errors if the reply can't be routed
+    socket.setsockopt(ZMQ_SNDTIMEO,
+        10 * 1000); // Set the timeout for sending replies to 10 seconds
 
     constexpr char const* const address = "tcp://*:22365";
     try
     {
-        MELINDA_TRACE_ALWAYS(trace_handle, "Binding to {}.", address);
+        MELINDA_TRACE_ALWAYS(trace_handle,
+            "Server will be registered on '{}' endpoint.",
+            address);
         socket.bind(address);
-        MELINDA_TRACE_ALWAYS(trace_handle, "Bound to {}.", address);
     }
     catch (zmq::error_t const& e)
     {
@@ -37,30 +41,74 @@ int main()
             "Can't bind to {}. Reason: ZMQ-{}:",
             e.num(),
             e.what());
+        std::terminate();
     }
     ON_SCOPE_EXIT(socket.unbind(address));
 
-    zmq::message_t message = zmq::message_t(1024);
-    zmq::message_t identity = zmq::message_t(1024);
-    zmq::message_t empty = zmq::message_t(1024);
     while (true)
     {
-        if (socket.recv(identity))
+        std::vector<zmq::message_t> request;
+        try
         {
-            if (socket.recv(empty))
+            zmq::recv_result_t const result = zmq::recv_multipart(socket,
+                std::back_inserter(request),
+                zmq::recv_flags::dontwait);
+            if (result)
             {
-                if (socket.recv(message))
-                {
-                    zmq::message_t send =
-                        zmq::message_t(address, sizeof("tcp://*:22365"));
+                // Process the message, otherwise if result is empty then no
+                // messages were queued on the socket
+                MELINDA_TRACE_INFO(trace_handle,
+                    "Received {} messages of {} bytes in size from {}.",
+                    request.size(),
+                    result.value(),
+                    request[0].to_string_view());
+            }
+        }
+        catch (zmq::error_t const& ex)
+        {
+            MELINDA_TRACE_FATAL(trace_handle,
+                "Unexpected error while receiving request. Reason: ZMQ-{}:",
+                ex.num(),
+                ex.what());
+        }
 
-                    std::cout
-                        << "Recevied message: " << message.to_string_view()
-                        << '\n';
-                    socket.send(identity, zmq::send_flags::sndmore);
-                    socket.send(empty, zmq::send_flags::sndmore);
-                    socket.send(send, zmq::send_flags::none);
-                }
+        if (request.empty())
+        {
+            continue;
+        }
+
+        try
+        {
+            std::vector<zmq::message_t> response;
+            response.emplace_back(std::move(request[0]));
+            response.emplace_back();
+            response.emplace_back(address, sizeof("tcp://*:22365"));
+            MELINDA_TRACE_INFO(trace_handle,
+                "Sending response of {} bytes to {}.",
+                response[2].size(),
+                response[0].to_string_view());
+            zmq::send_result_t const result = zmq::send_multipart(socket,
+                std::move(response),
+                zmq::send_flags::dontwait);
+            if (!result)
+            {
+                MELINDA_TRACE_ERROR(trace_handle, "Can't send response", "");
+            }
+        }
+        catch (zmq::error_t const& ex)
+        {
+            if (ex.num() == EHOSTUNREACH)
+            {
+                MELINDA_TRACE_WARN(trace_handle,
+                    "Client has disconnected or is not known.",
+                    "");
+            }
+            else
+            {
+                MELINDA_TRACE_FATAL(trace_handle,
+                    "Unexpected error while receiving request. Reason: ZMQ-{}:",
+                    ex.num(),
+                    ex.what());
             }
         }
     }
