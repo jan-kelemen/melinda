bcdb8468c6b0ab8cec396ab066e8cb6494d6d31b
Jan Kelemen <jkeleme@gmail.com>
2020-10-29 18:43:52 +0100

    Add concepts support to iterator_traits
    - Remove atleast_*_iterator declarations
    - Add concepts for iterator types

--- source/common/cppex/include/iterator_traits.h
+++ source/common/cppex/include/iterator_traits.h
@@ -8,38 +8,19 @@
 
 namespace mel::cppex
 {
+    template<typename Iterator>
+    using iterator_category =
+        typename std::iterator_traits<Iterator>::iterator_category;
+
     namespace detail
     {
-        template<typename Iterator>
-        using iterator_category =
-            typename std::iterator_traits<Iterator>::iterator_category;
-
-        template<typename Iterator, typename Category>
-        struct atleast_of_iterator_category final
-            : std::bool_constant<
-                  std::is_convertible_v<iterator_category<Iterator>, Category>>
-        {
-        public: // Types
-            using class_name = atleast_of_iterator_category;
-        };
-
         template<typename Iterator, typename Category>
         struct is_of_iterator_category final
             : std::bool_constant<
-                  std::is_same_v<iterator_category<Iterator>, Category>>
+                  std::is_convertible_v<iterator_category<Iterator>, Category>>
         {
-        public: // Types
-            using class_name = is_of_iterator_category;
         };
     } // namespace detail
-
-    template<typename Iterator, typename Category>
-    using atleast_of_iterator_category =
-        detail::atleast_of_iterator_category<Iterator, Category>;
-
-    template<typename Iterator, typename Category>
-    inline constexpr bool atleast_of_iterator_category_v =
-        atleast_of_iterator_category<Iterator, Category>::value;
 
     template<typename Iterator, typename Category>
     using is_of_iterator_category =
@@ -50,14 +31,6 @@ namespace mel::cppex
         is_of_iterator_category<Iterator, Category>::value;
 
     template<typename Iterator>
-    using atleast_input_iterator =
-        atleast_of_iterator_category<Iterator, std::input_iterator_tag>;
-
-    template<typename Iterator>
-    inline constexpr bool atleast_input_iterator_v =
-        atleast_input_iterator<Iterator>::value;
-
-    template<typename Iterator>
     using is_input_iterator =
         is_of_iterator_category<Iterator, std::input_iterator_tag>;
 
@@ -66,12 +39,7 @@ namespace mel::cppex
         is_input_iterator<Iterator>::value;
 
     template<typename Iterator>
-    using atleast_forward_iterator =
-        atleast_of_iterator_category<Iterator, std::forward_iterator_tag>;
-
-    template<typename Iterator>
-    inline constexpr bool atleast_forward_iterator_v =
-        atleast_forward_iterator<Iterator>::value;
+    concept input_iterator = std::input_iterator<Iterator>;
 
     template<typename Iterator>
     using is_forward_iterator =
@@ -82,12 +50,7 @@ namespace mel::cppex
         is_forward_iterator<Iterator>::value;
 
     template<typename Iterator>
-    using atleast_bidirectional_iterator =
-        atleast_of_iterator_category<Iterator, std::bidirectional_iterator_tag>;
-
-    template<typename Iterator>
-    inline constexpr bool atleast_bidirectional_iterator_v =
-        atleast_bidirectional_iterator<Iterator>::value;
+    concept forward_iterator = std::forward_iterator<Iterator>;
 
     template<typename Iterator>
     using is_bidirectional_iterator =
@@ -98,12 +61,7 @@ namespace mel::cppex
         is_bidirectional_iterator<Iterator>::value;
 
     template<typename Iterator>
-    using atleast_random_access_iterator =
-        atleast_of_iterator_category<Iterator, std::random_access_iterator_tag>;
-
-    template<typename Iterator>
-    inline constexpr bool atleast_random_access_iterator_v =
-        atleast_random_access_iterator<Iterator>::value;
+    concept bidirectional_iterator = std::bidirectional_iterator<Iterator>;
 
     template<typename Iterator>
     using is_random_access_iterator =
@@ -113,15 +71,19 @@ namespace mel::cppex
     inline constexpr bool is_random_access_iterator_v =
         is_random_access_iterator<Iterator>::value;
 
-    // TODO C++20: contiguous_iterator_tag
+    template<typename Iterator>
+    concept random_access_iterator = std::random_access_iterator<Iterator>;
 
     template<typename Iterator>
-    using atleast_output_iterator =
-        atleast_of_iterator_category<Iterator, std::output_iterator_tag>;
+    using is_contiguous_iterator =
+        is_of_iterator_category<Iterator, std::contiguous_iterator_tag>;
 
     template<typename Iterator>
-    inline constexpr bool atleast_output_iterator_v =
-        atleast_output_iterator<Iterator>::value;
+    inline constexpr bool is_contiguous_iterator_v =
+        is_contiguous_iterator<Iterator>::value;
+
+    template<typename Iterator>
+    concept contiguous_iterator = std::contiguous_iterator<Iterator>;
 
     template<typename Iterator>
     using is_output_iterator =
@@ -130,6 +92,9 @@ namespace mel::cppex
     template<typename Iterator>
     inline constexpr bool is_output_iterator_v =
         is_output_iterator<Iterator>::value;
+
+    template<typename Iterator, typename Value>
+    concept output_iterator = std::output_iterator<Iterator, Value>;
 } // namespace mel::cppex
 
 #endif
--- source/common/cppex/test/iterator_traits.t.cpp
+++ source/common/cppex/test/iterator_traits.t.cpp
@@ -13,164 +13,53 @@ namespace
     using bidirectional_iterator = std::list<char>::const_iterator;
     using random_access_iterator = std::vector<char>::const_iterator;
     using output_iterator = std::ostream_iterator<char>;
-
-    template<typename T, typename Enable = void>
-    class class_sfinae final
-    {
-    public: // Constants
-        static constexpr uint8_t value = 1;
-
-    public: // Types
-        using class_name = class_sfinae;
-    };
-
-    template<typename T>
-    class class_sfinae<T,
-        std::enable_if_t<mel::cppex::is_random_access_iterator_v<T>>>
-        final
-    {
-    public: // Constants
-        static constexpr uint8_t value = 2;
-
-    public: // Types
-        using class_name = class_sfinae;
-    };
-
-    template<typename T>
-    class class_sfinae<T, std::enable_if_t<mel::cppex::is_output_iterator_v<T>>>
-        final
-    {
-    public: // Constants
-        static constexpr uint8_t value = 3;
-
-    public: // Types
-        using class_name = class_sfinae;
-    };
-
-    template<typename T,
-        std::enable_if_t<mel::cppex::is_input_iterator_v<T>, int> = 0>
-    [[nodiscard]] constexpr uint8_t method_sfinae([[maybe_unused]] T&& it)
-    {
-        return uint8_t(1);
-    }
-
-    template<typename T,
-        std::enable_if_t<mel::cppex::is_random_access_iterator_v<T>, int> = 0>
-    [[nodiscard]] constexpr uint8_t method_sfinae([[maybe_unused]] T&& it)
-    {
-        return uint8_t(2);
-    }
 } // namespace
 
 TEST_CASE("Input iterator tests")
 {
-    using namespace mel::cppex;
-
-    static_assert(atleast_input_iterator_v<input_iterator>);
-    static_assert(is_input_iterator_v<input_iterator>);
-
-    static_assert(!atleast_forward_iterator_v<input_iterator>);
-    static_assert(!is_forward_iterator_v<input_iterator>);
-
-    static_assert(!atleast_bidirectional_iterator_v<input_iterator>);
-    static_assert(!is_bidirectional_iterator_v<input_iterator>);
-
-    static_assert(!atleast_random_access_iterator_v<input_iterator>);
-    static_assert(!is_random_access_iterator_v<input_iterator>);
-
-    static_assert(!atleast_output_iterator_v<input_iterator>);
-    static_assert(!is_output_iterator_v<input_iterator>);
+    static_assert(mel::cppex::is_input_iterator_v<input_iterator>);
+    static_assert(!mel::cppex::is_forward_iterator_v<input_iterator>);
+    static_assert(!mel::cppex::is_bidirectional_iterator_v<input_iterator>);
+    static_assert(!mel::cppex::is_random_access_iterator_v<input_iterator>);
+    static_assert(!mel::cppex::is_output_iterator_v<input_iterator>);
 }
 
 TEST_CASE("Forward iterator tests")
 {
-    using namespace mel::cppex;
-
-    static_assert(atleast_input_iterator_v<forward_iterator>);
-    static_assert(!is_input_iterator_v<forward_iterator>);
-
-    static_assert(atleast_forward_iterator_v<forward_iterator>);
-    static_assert(is_forward_iterator_v<forward_iterator>);
-
-    static_assert(!atleast_bidirectional_iterator_v<forward_iterator>);
-    static_assert(!is_bidirectional_iterator_v<forward_iterator>);
-
-    static_assert(!atleast_random_access_iterator_v<forward_iterator>);
-    static_assert(!is_random_access_iterator_v<forward_iterator>);
-
-    static_assert(!atleast_output_iterator_v<forward_iterator>);
-    static_assert(!is_output_iterator_v<forward_iterator>);
+    static_assert(mel::cppex::is_input_iterator_v<forward_iterator>);
+    static_assert(mel::cppex::is_forward_iterator_v<forward_iterator>);
+    static_assert(!mel::cppex::is_bidirectional_iterator_v<forward_iterator>);
+    static_assert(!mel::cppex::is_random_access_iterator_v<forward_iterator>);
+    static_assert(!mel::cppex::is_output_iterator_v<forward_iterator>);
 }
 
 TEST_CASE("Bidirectional iterator tests")
 {
-    using namespace mel::cppex;
-
-    static_assert(atleast_input_iterator_v<bidirectional_iterator>);
-    static_assert(!is_input_iterator_v<bidirectional_iterator>);
-
-    static_assert(atleast_forward_iterator_v<bidirectional_iterator>);
-    static_assert(!is_forward_iterator_v<bidirectional_iterator>);
-
-    static_assert(atleast_bidirectional_iterator_v<bidirectional_iterator>);
-    static_assert(is_bidirectional_iterator_v<bidirectional_iterator>);
-
-    static_assert(!atleast_random_access_iterator_v<bidirectional_iterator>);
-    static_assert(!is_random_access_iterator_v<bidirectional_iterator>);
-
-    static_assert(!atleast_output_iterator_v<bidirectional_iterator>);
-    static_assert(!is_output_iterator_v<bidirectional_iterator>);
+    static_assert(mel::cppex::is_input_iterator_v<bidirectional_iterator>);
+    static_assert(mel::cppex::is_forward_iterator_v<bidirectional_iterator>);
+    static_assert(
+        mel::cppex::is_bidirectional_iterator_v<bidirectional_iterator>);
+    static_assert(
+        !mel::cppex::is_random_access_iterator_v<bidirectional_iterator>);
+    static_assert(!mel::cppex::is_output_iterator_v<bidirectional_iterator>);
 }
 
 TEST_CASE("Random access iterator tests")
 {
-    using namespace mel::cppex;
-
-    static_assert(atleast_input_iterator_v<random_access_iterator>);
-    static_assert(!is_input_iterator_v<random_access_iterator>);
-
-    static_assert(atleast_forward_iterator_v<random_access_iterator>);
-    static_assert(!is_forward_iterator_v<random_access_iterator>);
-
-    static_assert(atleast_bidirectional_iterator_v<random_access_iterator>);
-    static_assert(!is_bidirectional_iterator_v<random_access_iterator>);
-
-    static_assert(atleast_random_access_iterator_v<random_access_iterator>);
-    static_assert(is_random_access_iterator_v<random_access_iterator>);
-
-    static_assert(!atleast_output_iterator_v<random_access_iterator>);
-    static_assert(!is_output_iterator_v<random_access_iterator>);
+    static_assert(mel::cppex::is_input_iterator_v<random_access_iterator>);
+    static_assert(mel::cppex::is_forward_iterator_v<random_access_iterator>);
+    static_assert(
+        mel::cppex::is_bidirectional_iterator_v<random_access_iterator>);
+    static_assert(
+        mel::cppex::is_random_access_iterator_v<random_access_iterator>);
+    static_assert(!mel::cppex::is_output_iterator_v<random_access_iterator>);
 }
 
 TEST_CASE("Output iterator tests")
 {
-    using namespace mel::cppex;
-
-    static_assert(!atleast_input_iterator_v<output_iterator>);
-    static_assert(!is_input_iterator_v<output_iterator>);
-
-    static_assert(!atleast_forward_iterator_v<output_iterator>);
-    static_assert(!is_forward_iterator_v<output_iterator>);
-
-    static_assert(!atleast_bidirectional_iterator_v<output_iterator>);
-    static_assert(!is_bidirectional_iterator_v<output_iterator>);
-
-    static_assert(!atleast_random_access_iterator_v<output_iterator>);
-    static_assert(!is_random_access_iterator_v<output_iterator>);
-
-    static_assert(atleast_output_iterator_v<output_iterator>);
-    static_assert(is_output_iterator_v<output_iterator>);
-}
-
-TEST_CASE("Iterator traits can be used for class SFINAE")
-{
-    static_assert(class_sfinae<forward_iterator>::value == uint8_t(1));
-    static_assert(class_sfinae<random_access_iterator>::value == uint8_t(2));
-    static_assert(class_sfinae<output_iterator>::value == uint8_t(3));
-}
-
-TEST_CASE("Iterator traits can be used for method SFINAE")
-{
-    static_assert(method_sfinae(input_iterator()) == uint8_t(1));
-    static_assert(method_sfinae(random_access_iterator()) == uint8_t(2));
+    static_assert(!mel::cppex::is_input_iterator_v<output_iterator>);
+    static_assert(!mel::cppex::is_forward_iterator_v<output_iterator>);
+    static_assert(!mel::cppex::is_bidirectional_iterator_v<output_iterator>);
+    static_assert(!mel::cppex::is_random_access_iterator_v<output_iterator>);
+    static_assert(mel::cppex::is_output_iterator_v<output_iterator>);
 }
