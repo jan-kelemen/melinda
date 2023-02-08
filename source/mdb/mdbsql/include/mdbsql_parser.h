69c69e2e4b11a825f8d6e375caf009ca18c08acd
Jan Kelemen <jkeleme@gmail.com>
2023-02-08 18:29:38 +0100

    Implement parsing up to column definitions

--- source/mdb/mdbsql/include/mdbsql_parser.h
+++ source/mdb/mdbsql/include/mdbsql_parser.h
@@ -1,13 +1,156 @@
 #ifndef MELINDA_MDBSQL_PARSER_INCLUDED
 #define MELINDA_MDBSQL_PARSER_INCLUDED
 
+#include <algorithm>
+#include <lexy/dsl/brackets.hpp>
+#include <lexy/dsl/separator.hpp>
+#include <vector>
+
+#include <lexy/callback/container.hpp>
+#include <lexy/callback/fold.hpp>
+#include <lexy/callback/string.hpp>
+#include <lexy/dsl.hpp>
+#include <lexy/dsl/ascii.hpp>
+#include <lexy/dsl/case_folding.hpp>
+#include <lexy/dsl/follow.hpp>
+#include <lexy/dsl/identifier.hpp>
+#include <lexy/dsl/newline.hpp>
+#include <lexy/dsl/option.hpp>
+#include <lexy/dsl/punctuator.hpp>
+#include <lexy/dsl/times.hpp>
+#include <lexy/dsl/unicode.hpp>
+#include <lexy/encoding.hpp>
+#include <lexy/input/buffer.hpp>
+
 #include <mdbsql_ast.h>
 
-namespace melinda::mdbsql::parser
+namespace melinda::mdbsql::grammar
 {
-} // namespace melinda::mdbsql::parser
+    namespace dsl = lexy::dsl;
 
-namespace melinda::mdbsql
-{
-} // namespace melinda::mdbsql
+    constexpr auto separator = LEXY_LIT("/*") >> dsl::until(LEXY_LIT("*/")) |
+        LEXY_LIT("--") >> dsl::until(dsl::newline) | dsl::unicode::space;
+
+    constexpr auto kw =
+        dsl::identifier(dsl::ascii::alpha, dsl::ascii::alpha_underscore);
+
+    struct identifier_part
+    {
+        static constexpr auto rule = []
+        {
+            auto lead_char = dsl::ascii::alpha;
+            auto trailing_char = dsl::ascii::alpha_underscore;
+            auto id = dsl::identifier(lead_char, trailing_char);
+
+            return id.reserve(
+                dsl::ascii::case_folding(LEXY_KEYWORD(u8"authorization", kw)));
+        }();
+
+        static constexpr auto value =
+            lexy::as_string<std::string, lexy::utf8_encoding>;
+    };
+
+    struct identifier
+    {
+        static constexpr auto rule = []
+        { return dsl::list(dsl::p<identifier_part>, dsl::sep(dsl::period)); }();
+
+        static constexpr auto value = lexy::fold_inplace<ast::identifier>(
+            ast::identifier {},
+            [](auto& rv, std::string i) { rv.parts.push_back(std::move(i)); });
+    };
+
+    struct column_definition
+    {
+        static constexpr auto rule = []
+        { return dsl::p<identifier> + dsl::p<identifier_part>; }();
+        static constexpr auto value = lexy::construct<ast::column_definition>;
+    };
+
+    struct table_elements
+    {
+        static constexpr auto rule = []
+        {
+            return dsl::parenthesized.list(dsl::p<column_definition>,
+                dsl::sep(dsl::comma));
+        }();
+
+        static constexpr auto value =
+            lexy::fold_inplace<ast::table_elements>({},
+                [](auto& rv, ast::column_definition v)
+                { rv.elements.push_back(std::move(v)); });
+    };
+
+    struct table_definition
+    {
+        static constexpr auto rule = []
+        {
+            return dsl::ascii::case_folding(LEXY_KEYWORD(u8"create", kw)) >>
+                dsl::opt(dsl::p<identifier_part> >>
+                    dsl::ascii::case_folding(LEXY_KEYWORD(u8"temporary", kw))) +
+                dsl::ascii::case_folding(LEXY_KEYWORD(u8"table", kw)) +
+                dsl::p<identifier> + dsl::p<table_elements>;
+        }();
+        static constexpr auto value = lexy::construct<ast::table_definition>;
+    };
+
+    struct path_specification
+    {
+        static constexpr auto rule = []
+        { return dsl::list(dsl::p<identifier>, dsl::sep(dsl::comma)); }();
+
+        static constexpr auto value =
+            lexy::fold_inplace<std::vector<ast::identifier>>({},
+                [](auto& rv, ast::identifier v)
+                { rv.push_back(std::move(v)); });
+    };
+
+    struct schema_definition
+    {
+        static constexpr auto rule = []
+        {
+            auto authorization =
+                dsl::ascii::case_folding(LEXY_KEYWORD(u8"authorization", kw)) >>
+                dsl::p<identifier_part>;
+
+            auto character_set =
+                dsl::ascii::case_folding(LEXY_KEYWORD(u8"default", kw)) >>
+                dsl::ascii::case_folding(LEXY_KEYWORD(u8"character", kw)) >>
+                dsl::ascii::case_folding(LEXY_KEYWORD(u8"set", kw)) >>
+                dsl::p<identifier>;
+            auto path_spec =
+                dsl::ascii::case_folding(LEXY_KEYWORD(u8"paths", kw)) >>
+                dsl::p<path_specification>;
+
+            return dsl::ascii::case_folding(LEXY_KEYWORD(u8"create", kw)) >>
+                dsl::ascii::case_folding(LEXY_KEYWORD(u8"schema", kw)) >>
+                (dsl::ascii::case_folding(
+                     LEXY_KEYWORD(u8"authorization", kw)) >>
+                        dsl::nullopt + dsl::p<identifier_part> |
+                    dsl::else_ >>
+                        dsl::p<identifier> + dsl::opt(authorization)) +
+                dsl::opt(character_set | path_spec) +
+                dsl::opt(character_set | path_spec) +
+                dsl::opt(dsl::p<table_definition>);
+        }();
+
+        static constexpr auto value = lexy::construct<ast::schema_definition>;
+    };
+
+    struct sql_schema_statement
+    {
+        static constexpr auto rule = dsl::p<schema_definition>;
+        static constexpr auto value =
+            lexy::construct<ast::sql_schema_statement>;
+    };
+
+    struct sql_executable_statement
+    {
+        static constexpr auto whitespace = separator;
+        static constexpr auto rule = dsl::p<sql_schema_statement>;
+        static constexpr auto value =
+            lexy::construct<ast::sql_executable_statement>;
+    };
+} // namespace melinda::mdbsql::grammar
+
 #endif
--- source/mdb/mdbsql/test/mdbsql_parser.t.cpp
+++ source/mdb/mdbsql/test/mdbsql_parser.t.cpp
@@ -1,11 +1,34 @@
-#include "mdbsql_ast.h"
 #include <catch2/catch_test_macros.hpp>
 
+#include <lexy/action/parse.hpp>
+#include <lexy/action/trace.hpp>
+#include <lexy/encoding.hpp>
+#include <lexy/input/range_input.hpp>
+#include <lexy_ext/report_error.hpp>
+
+#include <mdbsql_ast.h>
 #include <mdbsql_parser.h>
 
 using namespace melinda;
 
 TEST_CASE("<SQL executable statement>", "[sql-grammar]")
 {
-    std::string aaa {"aaa"};
+    std::u8string const input {
+        u8"CREATE/* fdsa*/SCHEMA melinda.mirko.kirko AUTHORIZATion mirko PATHS melinda.mirko, melinda.kirko default character set melinda.en_US CREATE global temporary table krokodil.mirko (foobar int, truebar varchar, kooobar boolean, kib.fda real)"};
+    lexy::range_input<lexy::utf8_encoding, std::u8string::const_iterator> const
+        range {std::cbegin(input), std::cend(input)};
+
+    lexy::trace<mdbsql::grammar::sql_executable_statement>(stdout, range);
+    auto const result =
+        lexy::parse<mdbsql::grammar::sql_executable_statement>(range,
+            lexy_ext::report_error);
+    REQUIRE(result.is_success());
+    REQUIRE(result.has_value());
+
+    auto const& schema_definition = std::get<mdbsql::ast::schema_definition>(
+        std::get<mdbsql::ast::sql_schema_statement>(result.value()));
+
+    INFO(schema_definition.schema_name.value().parts[0]);
+    REQUIRE(schema_definition.schema_name.value().parts[0] == "melinda");
+    REQUIRE(schema_definition.authorization == "mirko");
 }
