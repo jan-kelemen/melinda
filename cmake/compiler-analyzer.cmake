ca4d288e67b925dc4dffb3caaec4bcf628034c2e
Jan Kelemen <jkeleme@gmail.com>
2024-02-10 18:32:30 +0100

    Change gcc-analyzer to be active only on C code

--- cmake/compiler-analyzer.cmake
+++ cmake/compiler-analyzer.cmake
@@ -1,19 +1,14 @@
 add_library(compiler-analyzer INTERFACE)
 
-set(MSVC_ANALYZER
-    /analyze /analyze:external-)
-
-set(GCC_ANALYZER
-    -fanalyzer)
-
 if(MSVC)
-    set(ANALYZER_OPTIONS ${MSVC_ANALYZER})
+    target_compile_options(compiler-analyzer
+        INTERFACE
+        $<$<COMPILE_LANGUAGE:C,CXX>:/analyze /analyze:external->)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set(ANALYZER_OPTIONS ${GCC_ANALYZER})
+    target_compile_options(compiler-analyzer
+        INTERFACE
+        $<$<COMPILE_LANGUAGE:C>:-fanalyze>)
 else()
     message(AUTHOR_WARNING "No compiler analyzer options for CXX compiler: '${CMAKE_CXX_COMPILER_ID}'")
 endif()
-
-target_compile_options(compiler-analyzer
-    INTERFACE $<$<COMPILE_LANGUAGE:C,CXX>:${ANALYZER_OPTIONS}>)
 
