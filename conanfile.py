e8ad62a9542507c59b1d8498f9c1e45a4cc9dd23
Jan Kelemen <jkeleme@gmail.com>
2024-02-20 18:20:32 +0100

    Make conan recipe exportable

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,25 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
+require_conan_version = ">=2.0"
 
 class DanaConan(ConanFile):
     name = "melinda"
     settings = "os", "compiler", "build_type", "arch"
-    build_policy = "missing"
+    license = "BSD-3-Clause"
+    author = "Jan Kelemen"
+    url = "https://github.com/jan-kelemen/melinda"
+    version = "0.1"
+
+    exports_sources = "cmake", "src", "CMakeLists.txt", "LICENSE"
 
     def requirements(self):
         self.requires("foonathan-lexy/2022.12.1")
         self.requires("tl-expected/20190710")
 
     def build_requirements(self):
-        self.test_requires("catch2/3.5.2")
+        self.tool_requires("cmake/[^3.27]")
+        self.test_requires("catch2/[^3.5.2]")
 
     def layout(self):
         cmake_layout(self)
@@ -23,3 +31,7 @@ class DanaConan(ConanFile):
         cmake = CMakeDeps(self)
         cmake.generate()
 
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
