28f410913ca075c369e0cbfe81fadbb55bac3b07
Jan Kelemen <jkeleme@gmail.com>
2024-03-02 15:02:58 +0100

    Run clang tidy only with clang compiler

--- cmake/compiler-analyzer.cmake
+++ cmake/compiler-analyzer.cmake
@@ -3,11 +3,11 @@ add_library(compiler-analyzer INTERFACE)
 if(MSVC)
     target_compile_options(compiler-analyzer
         INTERFACE
-        $<$<COMPILE_LANGUAGE:C,CXX>:/analyze /analyze:external->)
+            $<$<COMPILE_LANGUAGE:C,CXX>:/analyze /analyze:external->)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     target_compile_options(compiler-analyzer
         INTERFACE
-        $<$<COMPILE_LANGUAGE:C>:-fanalyze>)
+            $<$<COMPILE_LANGUAGE:C>:-fanalyze>)
 else()
     message(AUTHOR_WARNING "No compiler analyzer options for CXX compiler: '${CMAKE_CXX_COMPILER_ID}'")
 endif()
--- src/mbl/mblcxx/include/mblcxx_expected.hpp
+++ src/mbl/mblcxx/include/mblcxx_expected.hpp
@@ -1,21 +1,17 @@
 #ifndef MELINDA_MBLCXX_EXPECTED_INCLUDED
 #define MELINDA_MBLCXX_EXPECTED_INCLUDED
 
-#ifdef __has_include
-#if __has_include(<expected>)
-#ifdef __clang__
+#ifdef MELINDA_MBLCXX_USE_TL_EXPECTED
 #include <tl/expected.hpp> // IWYU pragma: export
 #else
 #include <expected> // IWYU pragma: export
-#endif
-#endif
 #endif
 
 #include <system_error> // IWYU pragma: export
 
 namespace melinda::mblcxx
 {
-#ifdef __clang__
+#ifdef MELINDA_MBLCXX_USE_TL_EXPECTED
     using tl::expected;
     using tl::unexpected;
 #else
